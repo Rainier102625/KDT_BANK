9e181ac6c1edd630c9e0e8761a4fac3c4296583c
eunji56 <mefdllg2g35@gmail.com>
2025-08-04 13:13:49 +0900

    친구 초대 ui

--- src/main/java/com/example/bankservice1/controller/friendaddController.java
+++ src/main/java/com/example/bankservice1/controller/friendaddController.java
@@ -1,17 +1,31 @@
 package com.example.bankservice1.controller;
 
+import com.example.bankservice1.model.Friend;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class friendaddController {
-    @FXML private ListView<String> friendlist;
+    @FXML private TableView<Friend> friendTable;
+    @FXML private TableColumn<Friend,String> name;
+    @FXML private TableColumn<Friend,String> department;
+    @FXML private TableColumn<Friend,String> rank;
+
     @FXML
-    public void initialize(){
-        friendlist.setItems(FXCollections.observableArrayList(
-                "홍길동","조원","김철수"
-        ));
-        friendlist.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE); //friendlist 다중 선택 가능하게 설정
+    public void initialize() {
+        name.setCellValueFactory(new PropertyValueFactory<>("name"));
+        department.setCellValueFactory(new PropertyValueFactory<>("department"));
+        rank.setCellValueFactory(new PropertyValueFactory<>("rank"));
+
+        ObservableList<Friend> friendList = FXCollections.observableArrayList(
+                new Friend("박우현", "영업부", "대리" ),
+                new Friend("조대원", "영업부", "사원" )
+        );
+        friendTable.setItems(friendList);
     }
 }
