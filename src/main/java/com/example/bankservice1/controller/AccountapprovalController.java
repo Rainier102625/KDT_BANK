c67d6bbf758678ebcd88639cc43446f426acae5c
eunji56 <mefdllg2g35@gmail.com>
2025-08-07 10:22:44 +0900

    계좌 생성 승인

--- src/main/java/com/example/bankservice1/controller/AccountapprovalController.java
+++ src/main/java/com/example/bankservice1/controller/AccountapprovalController.java
@@ -8,6 +8,7 @@ import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -24,16 +25,18 @@ import java.util.List;
 public class AccountapprovalController {
     @FXML private VBox requestListContainer;
     @FXML private TextField SearchName;
+    @FXML private ComboBox<String> accountRequest;
 
     private final ObjectMapper objectMapper = new ObjectMapper();
     private final HttpClient httpClient = HttpClient.newHttpClient();
+
     @FXML
     private void initialize() {
         handleSearchAccount();
     }
     private void handleSearchAccount() {
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(apiconstants.BASE_URL+"/accounts/pending"))
+                .uri(URI.create(apiconstants.BASE_URL+"/account-request/pending"))
                 .header("Authorization", "Bearer " + tokenManager.getInstance().getJwtToken())
                 .GET()
                 .build();
@@ -46,25 +49,23 @@ public class AccountapprovalController {
                             List<Account> accountList = Arrays.asList(accountArray);
 
                             String query = SearchName.getText();
+                            String query2 = accountRequest.getValue();
                             List<Account> filterList = query.isEmpty() ? accountList :
                                     accountList.stream()
-                                            .filter(account -> account.getCustomerName().contains(query))
+                                            .filter(account -> account.getCustomerName().contains(query) && account.getRequestType().equals(query2))
                                             .toList();
                             Platform.runLater(() -> {
                                 requestListContainer.getChildren().clear();
 
                                 for(Account account : filterList){
                                     String status;
-                                    switch (account.getAccountStatus()) {
-                                        case "PENDING" -> status = "생성";
-                                        case "DELETE_PENDING" -> status ="삭제";
+                                    switch (account.getRequestType()) {
+                                        case "CREATE" -> status = "생성";
+                                        case "DELETE" -> status ="삭제";
                                         default -> status = "알 수 없음";
                                     }
                                     addRequestCard(
-                                            account.getCustomerName(),
-                                            account.getCustomerBirth(),
-                                            account.getCustomerBirth(),
-                                            account.getProductName(),
+                                            account,
                                             status
                                     );
                                 }
@@ -81,8 +82,53 @@ public class AccountapprovalController {
                     return null;
                 });
     }
+    private void handleApprove(int requestIndex) {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(apiconstants.BASE_URL+"/account-request/"+requestIndex + "/approve"))
+                .header("Authorization", "Bearer " + tokenManager.getInstance().getJwtToken())
+                .POST(HttpRequest.BodyPublishers.noBody()) //요청 본문 없음
+                .build();
+        httpClient.sendAsync(request, java.net.http.HttpResponse.BodyHandlers.ofString())
+                .thenAccept(response -> {
+                    if (response.statusCode() == 200) {
+                        System.out.println("success");
+                        Platform.runLater(() -> this.handleSearchAccount());
+                    }else {
+                        System.out.println("fail");
+                        System.out.println(response.statusCode());
+                        System.out.println(requestIndex);
+                    }
+                })
+                .exceptionally(ex -> {
+                    ex.printStackTrace();
+                    return null;
+                });
+    }
 
-    private void addRequestCard(String name, String customerBirth, String accountNum, String productName, String accountStatus) {
+    private void handleReject(int requestIndex) {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(apiconstants.BASE_URL+"/account-request/"+requestIndex + "/reject"))
+                .header("Authorization", "Bearer " + tokenManager.getInstance().getJwtToken())
+                .POST(HttpRequest.BodyPublishers.noBody()) //요청 본문 없음
+                .build();
+        httpClient.sendAsync(request, java.net.http.HttpResponse.BodyHandlers.ofString())
+                .thenAccept(response -> {
+                    if (response.statusCode() == 200) {
+                        System.out.println("success");
+                        Platform.runLater(() -> this.handleSearchAccount());
+                    }else {
+                        System.out.println("fail");
+                        System.out.println(response.statusCode());
+                        System.out.println(requestIndex);
+                    }
+                })
+                .exceptionally(ex -> {
+                    ex.printStackTrace();
+                    return null;
+                });
+    }
+    //요청 카드 디자인
+    private void addRequestCard(Account account, String status) {
         HBox card = new HBox(10);
         card.setStyle("-fx-background-color: white; -fx-background-radius: 10; -fx-padding: 5; -fx-border-color: #eeeeee; -fx-border-radius: 10;");
         card.setPrefWidth(560);
@@ -95,25 +141,27 @@ public class AccountapprovalController {
 
         VBox customerBox = new VBox(3,
                 new Label("고객정보") {{ setStyle("-fx-font-size: 12px; -fx-text-fill: #999;"); }},
-                new Label(name),
-                new Label(customerBirth)
+                new Label(account.getCustomerName()),
+                new Label(account.getCustomerBirth())
         );
         VBox accountBox = new VBox(3,
                 new Label("계좌") {{ setStyle("-fx-font-size: 12px; -fx-text-fill: #999;"); }},
-                new Label(accountNum),
-                new Label(productName)
-                );
-        Label statusLabel = new Label(accountStatus);
+                new Label(account.getAccountNum()),
+                new Label(account.getProductName())
+        );
+        Label statusLabel = new Label(status);
         statusLabel.setStyle("-fx-background-color: #fceabb; -fx-padding: 2 8; -fx-background-radius: 8;");
 
         VBox statusBox = new VBox(3,
                 new Label("요청") {{ setStyle("-fx-font-size: 12px; -fx-text-fill: #999;"); }},
                 new HBox(statusLabel)
-                );
+        );
         Button approve = new Button("승인");
         approve.setStyle("-fx-background-color: #3bb273; -fx-text-fill: white;");
+        approve.setOnAction(event -> handleApprove(account.getRequestIndex()));
         Button reject = new Button("거절");
         reject.setStyle("-fx-background-color: #f87171; -fx-text-fill: white;");
+        reject.setOnAction(event -> handleReject(account.getRequestIndex()));
         HBox buttonBox = new HBox(10, approve, reject);
         buttonBox.setAlignment(Pos.CENTER_RIGHT);
 
